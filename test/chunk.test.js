f9ee00d4f5d6d15dd6ce617483bfaff05a60e249
jsbisht <jagdeepbisht89@gmail.com>
2020-05-22 18:56:28 +0530

    Added object array chunking test

--- test/chunk.test.js
+++ test/chunk.test.js
@@ -1,7 +1,7 @@
 const chunk = require("../code/chunk");
 
 describe("test if chunk creation", () => {
-  test("basic test", () => {
+  test("integer array chunking", () => {
     const batch_size = 3;
     const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
     const chunks = chunk(arr, batch_size);
@@ -11,5 +11,39 @@ describe("test if chunk creation", () => {
     expect(chunks.next().value).toEqual(expect.arrayContaining([7, 8, 9]));
     expect(chunks.next().value).toEqual(expect.arrayContaining([10, 11, 12]));
     expect(chunks.next().value).toEqual(expect.arrayContaining([13]));
+  });
+
+  test("object array chunking", () => {
+    const batch_size = 3;
+    const arr = [
+      { x: 1 },
+      { x: 2 },
+      { y: 3 },
+      { x: 4 },
+      { x: 5 },
+      { x: 6 },
+      { x: 7 },
+      { x: 8 },
+      { x: 9 },
+      { x: 10 },
+      { x: 11 },
+      { x: 12 },
+      { x: 13 },
+    ];
+    const chunks = chunk(arr, batch_size);
+
+    expect(chunks.next().value).toEqual(
+      expect.arrayContaining([{ x: 1 }, { x: 2 }, { y: 3 }])
+    );
+    expect(chunks.next().value).toEqual(
+      expect.arrayContaining([{ x: 4 }, { x: 5 }, { x: 6 }])
+    );
+    expect(chunks.next().value).toEqual(
+      expect.arrayContaining([{ x: 7 }, { x: 8 }, { x: 9 }])
+    );
+    expect(chunks.next().value).toEqual(
+      expect.arrayContaining([{ x: 10 }, { x: 11 }, { x: 12 }])
+    );
+    expect(chunks.next().value).toEqual(expect.arrayContaining([{ x: 13 }]));
   });
 });
